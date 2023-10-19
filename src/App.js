c3a6ce56cb86f8dd101097676b3be9520fd63aae
Prateek Singh <prateeksinghk7@gmail.com>
2023-10-19 16:13:48 +0530

    added component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-
+import Header from './components/header';
 function App() {
   return (
+   <>
    <div className="header">
+
     <div className="body">
       
     </div>
     <div className="footer"></div>
    </div>
+   <Header/>
+   </>
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+function Header(){
+return(
+    <><h1>hi it's me</h1></>
+);
+}
+
+export default Header();
